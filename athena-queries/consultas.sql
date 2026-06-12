b20965fc7b610b622b064c0ef6a6119a3c44cbe2
alvarocuba1922 <alvarocuba1922@gmail.com>
2026-06-11 19:11:16 -0500

    Update consultas.sql
    ultimas consultas realizadas

--- athena-queries/consultas.sql
+++ athena-queries/consultas.sql
@@ -1,31 +1,34 @@
 
-//
-CREATE EXTERNAL TABLE ticketsdb.tickets_clasificados (
+--crear la tabla
+  CREATE EXTERNAL TABLE ticketsdb.tickets_clasificados (
   ticket_id string,
   fecha_creada string,
   canal string,
   mensaje_usuario string,
   prioridad string
 )
-ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
-WITH SERDEPROPERTIES (
-  'separatorChar' = ',',
-  'quoteChar' = '"',
-  'escapeChar' = '\\'
-)
-STORED AS TEXTFILE
-LOCATION 's3://tickets-curated-grupo5/curated/'
-TBLPROPERTIES (
-  'skip.header.line.count'='1'
-);
-
-
-// 
+  
+--para llamar todos los registro en la tabla
 SELECT *
-FROM ticketsdb.tickets_clasificados;
+FROM ticketsdb.tickets_clasificados
 
+--para llamar por fecha
+SELECT *
+FROM ticketsdb.tickets_clasificados
+WHERE fecha_creada = '8/06/2026';
 
-//
+--EL TOTAL
 SELECT prioridad, COUNT(*) AS cantidad
 FROM ticketsdb.tickets_clasificados
-GROUP BY prioridad;
+GROUP BY prioridad
+ORDER BY cantidad DESC;
+
+--TOTAL POR FECHAS 
+SELECT fecha_creada,
+       COUNT(*) AS total_tickets
+FROM ticketsdb.tickets_clasificados
+GROUP BY fecha_creada
+ORDER BY fecha_creada;
+
+
+
